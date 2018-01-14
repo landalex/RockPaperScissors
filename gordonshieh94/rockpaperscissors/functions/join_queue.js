27f5de57c8ad60c274a99d902ca1b5bde29ffbb7
Gordon Shieh <gshieh@sfu.ca>
2018-01-13 21:15:05 -0800

    Add async helper methods

--- gordonshieh94/rockpaperscissors/functions/join_queue.js
+++ gordonshieh94/rockpaperscissors/functions/join_queue.js
@@ -3,21 +3,8 @@
 * @param {string} name Who you're saying hello to
 * @returns {string}
 */
-var redis = require('redis')
+var redis = require('../utils/redis')
 
-var client = redis.createClient('redis://34.211.214.70:6379')
-
-module.exports = (name = 'default', context, callback) => {
-  client.exists(name, (err, exists) => {
-    if (exists == 0) {
-      // Add name (uuid) to set
-      client.sadd('in_queue', name);
-      // Sets don't have an expiry, so we need to make a key with a value that expires
-      client.set(name, 'still alive!', 'EX', 10);
-      callback(null, `hello ${name}`);
-    } else {
-      callback(null, `get outta here ${name}`)
-    }
-
-  })
+module.exports = async (name = 'default', context) => {
+  return await redis.joinQueue(name)
 };
