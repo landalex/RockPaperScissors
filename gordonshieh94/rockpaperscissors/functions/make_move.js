0b76e26b5f43999558cd8f2218206d9304cacf41
Alex Land <aland@sfu.ca>
2018-01-13 23:34:35 -0800

    Add function for user move

---
+++ gordonshieh94/rockpaperscissors/functions/make_move.js
@@ -0,0 +1,14 @@
+var redis = require('../utils/redis');
+
+/**
+* Record a move.
+* @param {string} name The user making the move
+* @param {string} move The move being made ("rock", "paper", or "scissors")
+* @returns {string}
+*/
+
+module.exports = async (name, move) => {
+    var matchId = await redis.getMyMatch(name);
+    await redis.setMatchMove(name, matchId, move);
+    return matchId;
+};
